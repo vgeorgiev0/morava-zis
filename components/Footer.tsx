b82300c00f23429e61cc88b9eae2be730b224017
VGeorgiev <vgeorgiev@up2technology.com>
2022-12-16 11:11:25 +0200

    feat: Created the footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,67 @@
 import React from "react";
+import { MdOutlinePlace, MdOutlinePhone, MdOutlineEmail } from "react-icons/md";
+import { useTranslation } from "next-i18next";
+import { useRouter } from "next/router";
 
-type Props = {};
+const Footer = () => {
+  const { t } = useTranslation();
+  const { locale } = useRouter();
 
-const Footer = (props: Props) => {
+  const aboutInfo = [
+    {
+      title: t("address"),
+      icon: <MdOutlinePlace />,
+      description: t("addressDesc"),
+      info: "https://www.google.com/maps/place/Restoran+%C5%BDIS/@43.3877598,21.7627089,14z/data=!4m15!1m7!3m6!1s0x4755c99c33def2eb:0x9e67a37fc410aefd!2zTWV6Z3JhamEsINCh0YrRgNCx0LjRjw!3b1!8m2!3d43.3963872!4d21.7846362!3m6!1s0x4755c99ce220e257:0x7b56374b227c81f3!8m2!3d43.3927307!4d21.7826271!9m1!1b1",
+    },
+    {
+      title: t("phone"),
+      icon: <MdOutlinePhone />,
+      description: "+381 69 1797 316",
+      info: "tel:+381-691-797-316",
+    },
+    {
+      title: t("email"),
+      icon: <MdOutlineEmail />,
+      description: `slobodan.
+      zivadinovic
+      @gmail.com`,
+      info: "mailto:slobodan.zivadinovic@gmail.com",
+    },
+  ];
 
-  return <div></div>;
+  return (
+    <footer className="bg-slate-800 text-white shadow-xl w-full text-left font-bold text-base px-14 py-4">
+      <h1 className="text-center font-extrabold text-2xl pb-4">About us</h1>
+      <div className="bg-white w-full h-0.5 rounded-full self-center" />
+      <div className="flex justify-around gap-10 pt-6">
+        {aboutInfo.map((item, index) => {
+          return (
+            <article
+              key={index}
+              className={`flex flex-col items-center gap-2 md:ml-16 ${
+                locale === "en" ? "md:ml-0" : ""
+              }`}
+            >
+              <h6 className="text-lg select-none">{item.title}</h6>
+              <a
+                className="hover:opacity-100 opacity-80 hover:text-lg transition-all duration-500"
+                target="_blank"
+                rel="noopener noreferrer"
+                href={item.info}
+              >
+                <div className="items-center justify-center flex mb-1 hover:text-3xl text-2xl transition-all duration-500">
+                  {item.icon}
+                </div>
+                {item.description}
+              </a>
+            </article>
+          );
+        })}
+      </div>
+      <div>{/* V1rt */}</div>
+    </footer>
+  );
 };
 
 export default Footer;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -30,23 +30,23 @@ const Navbar = () => {
   return (
     <nav
       className={` flex justify-evenly  sticky w-full top-0 left-0 bg-white rounded-b-full rounded-tl-full transition-all duration-300  drop-shadow-2xl ${
-        isOpen ? "h-54 rounded-tl-3xl rounded-bl-full rounded-br-3xl" : ""
+        isOpen ? "h-54 rounded-tl-3xl rounded-bl-full rounded-br-3xl mt-1" : ""
       }`}
     >
       <div className="flex flex-row flex-1 gap-10 px-4 md:pl-10">
         <div className="flex flex-1 py-1">
-          <Link className=" mt-2" locale={locale} href={"/"} onClick={scroll}>
+          <Link className="mt-2" locale={locale} href={"/"} onClick={scroll}>
             <Image
               width={65}
               height={60}
-              className="w-16 h-20 md:w-24 md:h-32  flex-1 hover:opacity-80 transition-all duration-500"
+              className="w-14 h-16 md:w-16 md:h-20  flex-1 hover:opacity-80 transition-all duration-500"
               priority={true}
               alt="logo"
               src={logo}
             />
           </Link>
         </div>
-        <div className="flex flex-2 mr-10 flex-col transition-all duration-500">
+        <div className="flex flex-2 mr-4 flex-col transition-all duration-500">
           <div
             className={` flex  sm:flex-row flex-col transition-all duration-500 ${
               isOpen ? " flex-col " : "hidden md:flex"
@@ -81,7 +81,7 @@ const Navbar = () => {
               {t("about")}
             </Link>
           </div>
-          <div className="flex flex-1 gap-3 justify-end pt-6 pb-6 ">
+          <div className="flex flex-1 gap-1 justify-end pt-6 md:pt-0  ">
             {locales?.map((locale) => (
               <Link
                 className={`localeButton ${
@@ -106,7 +106,7 @@ const Navbar = () => {
           </div>
         </div>
         <div className="flex justify-self-end ">
-          <button className="inline md:hidden" onClick={handleToggle}>
+          <button className="inline md:hidden  " onClick={handleToggle}>
             <FaAlignRight className="hover:text-orange-400 text-amber-900 w-full h-full " />
           </button>
         </div>
